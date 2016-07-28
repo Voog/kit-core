165917773cba09f7da3be845ae6e67823dcdc0ca
Mikk Pristavka <mikk@voog.com>
2016-07-28 15:12:52 +0300

    Remove obsolete site-context module

--- src/core.js
+++ src/core.js
@@ -2,19 +2,15 @@
 
 import {version} from '../package.json';
 
-import fs from 'fs';
-
 import fileUtils from './file_utils';
 import config from './config';
 import sites from './sites';
 import actions from './actions';
-import {default as site} from './site_context';
 
 export default {
   fileUtils,
   config,
   sites,
   actions,
-  site,
   version
 };
